e2cf89f1fc69ee1369363edbdefdad229a75c10d
Sergei Pleshanov <2357qwr@gmail.com>
2022-12-02 23:26:05 +0300

    feat: add streams/write task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,23 @@
+import { open } from 'node:fs/promises';
+
+
 const write = async () => {
-    // Write your code here 
+  try {
+    const pathToFile = new URL('./files/fileToWrite.txt', import.meta.url);
+    const file = await open(pathToFile, 'w' );
+    const readableFromTerminal  = process.stdin;
+    const writableToFile = file.createWriteStream();
+
+    readableFromTerminal.pipe(writableToFile);
+    console.log('Enter "exit" to exit!');
+
+    readableFromTerminal.on('data' , chunk => {
+      const chunkStringify = chunk.toString();
+      if (chunkStringify.match('exit')) readableFromTerminal.unpipe(writableToFile);
+    });
+  } catch (err) {
+    console.error(`Error occurred: ${err}`);
+  }
 };
 
 await write();
