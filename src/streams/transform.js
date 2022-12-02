bf348827449f2b6e370bb3bc321ef1e21612a263
Sergei Pleshanov <2357qwr@gmail.com>
2022-12-02 23:57:25 +0300

    feat: add streams/transform task

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,30 @@
+import { pipeline } from 'node:stream/promises';
+import { Transform } from 'node:stream';
+
+
 const transform = async () => {
-    // Write your code here 
+  try {
+    const readableFromTerminal = process.stdin;
+    const writableToTerminal = process.stdout;
+
+    console.log('Enter "exit" to exit!');
+
+    const transform = new Transform({
+      transform(chunk, enc, cb) {
+        const chunkStringify = chunk.toString().trim();
+        if (chunkStringify.match('exit')) {
+          readableFromTerminal.unpipe(transform);
+        } else {
+          const reversedChunk = chunkStringify.split('').reverse().join('');
+          cb(null, reversedChunk + '\n');
+        }
+      }
+    });
+
+    await pipeline(readableFromTerminal, transform, writableToTerminal);
+  } catch (err) {
+    console.error(`Error occurred: ${err}`);
+  }
 };
 
 await transform();
