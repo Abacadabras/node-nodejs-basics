3005432fcfcf79c0143c959dece13d821915a070
Sergei Pleshanov <2357qwr@gmail.com>
2022-12-03 20:12:22 +0300

    feat: fs/read task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,14 @@
+import fs from 'node:fs/promises';
+
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const pathToFile = new URL('./files/fileToRead.txt', import.meta.url);
+    const dataToFile = await fs.readFile(pathToFile, { encoding: 'utf8'});
+    console.log(dataToFile);
+  } catch {
+      console.error(new Error('FS operation failed'));
+    }
 };
 
 await read();
