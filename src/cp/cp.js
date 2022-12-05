f2d39e8ae11254806f3c2c866f5fa55adfade7f4
Sergei Pleshanov <2357qwr@gmail.com>
2022-12-05 23:59:38 +0300

    feat: child processes task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,13 @@
+import { fork } from 'child_process';
+
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+
+  const pathToFile = new URL('./files/script.js', import.meta.url);
+  const child = fork(pathToFile, args, { silent: true });
+
+  process.stdin.pipe(child.stdin);
+  child.stdout.pipe(process.stdout);
 };
 
-spawnChildProcess();
+await spawnChildProcess(['first', 'second']);
