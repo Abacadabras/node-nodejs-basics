b97c003a6ffdb27360dc179d55e28dc5ef4c8093
Sergei Pleshanov <2357qwr@gmail.com>
2022-12-07 11:57:32 +0300

    feat: add Zlib task

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,16 +1,17 @@
 import { open } from "node:fs/promises";
 import { createGzip }  from 'node:zlib';
 import { pipeline } from 'node:stream/promises';
+import { getPathToFile } from "../lib/getPathToFile.js";
 
 
+const pathToReadFile = getPathToFile(import.meta.url, 'files', 'fileToCompress.txt');
+const pathToWriteFile = getPathToFile(import.meta.url, 'files', 'archive.gz');
 
 const compress = async () => {
   try {
-    const pathToReadFile = new URL('./files/fileToCompress.txt', import.meta.url);
     const fileRead = await open(pathToReadFile);
     const readableFromFile = fileRead.createReadStream();
 
-    const pathToWriteFile = new URL('./files/archive.gz', import.meta.url);
     const fileWrite = await open(pathToWriteFile, 'w');
     const writableToFile = fileWrite.createWriteStream();
 
@@ -18,7 +19,7 @@ const compress = async () => {
 
     await pipeline(readableFromFile, gzip, writableToFile);
   } catch (err) {
-      console.error(`Error occurred: ${err}`);
+    throw err;
   }
 };
 
