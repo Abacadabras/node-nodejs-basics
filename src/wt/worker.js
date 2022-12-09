d7dc47df988b4dc094417334c5cf1cc13a7eabe0
Sergei Pleshanov <2357qwr@gmail.com>
2022-12-09 13:53:50 +0300

    feat: add Worker Threads task

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -5,6 +5,7 @@ import { isMainThread, parentPort, workerData } from 'node:worker_threads';
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
+  if (Math.random() > 0.5) throw new Error('oops!'); // for check!!!
   if (!isMainThread) parentPort.postMessage(nthFibonacci(workerData));
 };
 
